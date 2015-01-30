81ed2ee1bbf59ce0eb9e2a69c9ef3d803b0ef93e
karan sharma <karan.sharma516@yahoo.com>
2015-01-29 18:09:06 -0800

    finish the project and make some changes in displaying the results

--- src/Tester.java
+++ src/Tester.java
@@ -3,24 +3,21 @@
  */
 
 /**
- * @author karan
+ * @author Karan Sharma
+ * Description: This class Tester tests the Hero class Here 
+ * @version: 1.2 (January 29, 2015)
  *
  */
 public class Tester {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		 Hero karan = new Hero("kay");
 	        //Display Hero's abilities
-	        karan.show();
-	        
-	        karan.fight();
-	       
-
+		 karan.show();
+	     karan.fight(); 
+	
 	}
 
 }
